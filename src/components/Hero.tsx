641e40dcd9dea9af11a81b085d5a395777e4fcfe
Ryan Trainor <rptrainor@gmail.com>
2024-06-30 10:49:38 -0300

    update hero section copy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,40 +1,34 @@
-// import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import Button from "./Button";
 
 const Hero = () => {
   return (
     <div className="relative isolate overflow-hidden bg-slate-800 rounded">
- 
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+
+      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 lg:flex lg:px-8">
         <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-slate-200 sm:text-6xl">
-            Your Solana wallet
+          <h1 className="mt-10 text-4xl font-bold tracking-tight text-slate-200 sm:text-6xl text-balance">
+            Send &#38; Receive SOL
           </h1>
           <p className="mt-6 text-lg leading-8 text-slate-300">
             Easily check your Solana wallet balance and send transactions with ease.
           </p>
           <div className="mt-10 flex items-center gap-x-6">
-            <a
-              href="/"
-              className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-slate-900 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            <button
+              type="button"
+              className="group/button relative inline-flex items-center justify-center overflow-hidden bg-yellow-500 py-1 px-2 rounded-full text-xs font-normal text-black transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-slate-600/30 border-black border-2"
             >
-              Get started
-            </a>
-            <a href="/" className="text-sm font-semibold leading-6 text-white">
-              Learn more <span aria-hidden="true">→</span>
+              <span className="text-lg font-bold">
+                Connect wallet
+              </span>
+              <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+                <div className="relative h-full w-8 bg-white/20" />
+              </div>
+            </button>
+            <a href="https://chromewebstore.google.com/detail/solflare-wallet/bhhhlbepdkbapadjdnnojkbgioiodbic" className="text-sm font-semibold leading-6 text-white">
+              Create a wallet <span aria-hidden="true">→</span>
             </a>
           </div>
         </div>
-        {/* <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width={2432}
-              height={1442}
-              className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-            />
-          </div>
-        </div> */}
       </div>
     </div>
   )
