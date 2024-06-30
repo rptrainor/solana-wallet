9dff8ab2d9d4a986128bb8e6617d31b83ce9cec1
Ryan Trainor <rptrainor@gmail.com>
2024-06-30 14:40:37 -0300

    improve formating

--- src/components/WalletDetails.tsx
+++ src/components/WalletDetails.tsx
@@ -22,17 +22,20 @@ const WalletDetails: React.FC = () => {
 				<Menu />
 				<div className="flex flex-col gap-4 grow">
 					<Hero />
-
 					{value !== "ERROR" && value !== "DISCONNECTED" && (
 						<dl className="grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center mx-auto max-w-4xl w-full">
-							{context.balance !== null && (<div className="flex flex-col bg-white/5 p-8 w-full">
-								<dd className="order-first text-3xl font-semibold tracking-tight text-white">{context.balance} SOL</dd>
-								<dt className="text-sm font-semibold leading-6 text-gray-300">balance</dt>
-							</div>)}
-							{value !== "CONNECTING" && (<div className="flex flex-col bg-white/5 p-8">
-								<dt className="text-sm font-semibold leading-6 text-gray-300">last transaction signature</dt>
-								<dd className="order-first text-3xl font-semibold tracking-tight text-white">{context.transactionSignature}</dd>
-							</div>)}
+							{context.balance !== null && (
+								<div className="flex flex-col bg-white/5 p-8 w-full">
+									<dd className="order-first text-3xl font-semibold tracking-tight text-white">{context.balance} SOL</dd>
+									<dt className="text-sm font-semibold leading-6 text-gray-300">balance</dt>
+								</div>
+							)}
+							{value !== "CONNECTING" && (
+								<div className="flex flex-col bg-white/5 p-8">
+									<dt className="text-sm font-semibold leading-6 text-gray-300">last transaction signature</dt>
+									<dd className="order-first text-3xl font-semibold tracking-tight text-white">{context.transactionSignature}</dd>
+								</div>
+							)}
 							<div className="flex flex-col bg-white/5 p-8">
 								<dt className="text-sm font-semibold leading-6 text-gray-300">connection status</dt>
 								<dd className="order-first text-3xl font-semibold tracking-tight text-white">{value}</dd>
